573e5ead42b4d4412689d899be25584e245a27ad
Noahw <noahwuwuwu@airbears2-10-142-116-148.airbears2.1918.berkeley.edu>
2019-06-03 18:49:24 -0700

    switched codebase to using react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import React, { Component } from "react";
-import "./App.css";
-import Gmap from "./Gmap.js";
-import Button from "./Button.js";
+import Leaflet from 'leaflet'
+import React, { StrictMode } from 'react'
+import SimpleExample from './components/simple.js'
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>header & navbar stuff go here</p>
-        </header> */}
-        
-        <Gmap ref={el => (this.mapComponent = el)}/>
-        
-        <button class="toggleButton"
-          onClick={() => {
-            if (this.mapComponent) {
-              this.mapComponent.toggleSat();
-            }
-          }}
-        >
-          Toggle Satellite
-        </button>
 
-        <button class="toggleFire"
-          onClick={() => {
-            if (this.mapComponent) {
-              this.mapComponent.toggleFire();
-            }
-          }}
-        >
-          Toggle Fire
-        </button>
-
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <SimpleExample></SimpleExample>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Gmap from'./Gmap';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const rootElement = <App />
-ReactDOM.render(rootElement, document.getElementById('root'));
-// ReactDOM.render(<Gmap />, document.getElementById('root'));
+const MOUNT_NODE = document.getElementById('app')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+  const App = require('./App').default
+  ReactDOM.render(<App />, MOUNT_NODE)
+}
+
+render()
+
+if (module.hot) {
+  module.hot.accept(['./App'], () =>
+    setImmediate(() => {
+      ReactDOM.unmountComponentAtNode(MOUNT_NODE)
+      render()
+    }),
+  )
+}
